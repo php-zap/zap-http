18b032670e9fcb2e81e7c9d1855de4ce4609edf8
picahielos <antonio.lopez.zapata@gmail.com>
2015-11-24 16:07:08 +0100

    Adding phpunit.xml.dist and empty Request.

---
+++ src/Request.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Zap\Http;
+
+/**
+ * Representation of a HTTP request.
+ * @package Zap\Http
+ * @author Antonio Lopez <antonio.lopez.zapata@gmail.com>
+ */
+class Request
+{
+}
---
+++ tests/Zap/Http/RequestTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Test\Zap\Http;
+
+use PHPUnit_Framework_TestCase;
+
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Dump test, just to have one.
+     * @todo remove this test
+     */
+    public function testClassExists()
+    {
+        $this->assertTrue(class_exists('\Zap\Http\Request'));
+    }
+}
